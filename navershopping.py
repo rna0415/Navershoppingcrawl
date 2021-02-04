247556d1bad913bb099260e5cda399ad1fa894fb
Moses <67581910+rna0415@users.noreply.github.com>
2021-02-04 15:34:29 +0900

    Update and rename crawl-ipccategory1.py to navershopping.py

--- navershopping.py
+++ navershopping.py
@@ -2,16 +2,13 @@ from urllib import request
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import selenium
-import konlpy
-from konlpy.tag import Kkma
 import csv
 from selenium import webdriver
 import time
 import json
 from selenium.webdriver.common.keys import Keys
 
-cosmetic = ['펫케어'] 
-# ,'노트북','키보드','모니터']
+cosmetic = ['키워드 입력'] #리스트 형식으로 다수의 키워드 입력가능; ['노트북','키보드','모니터']
 
 
 url = 'https://shopping.naver.com/'
@@ -25,20 +22,22 @@ driver.get(url)
 
 for typen in cosmetic:
 
-    csvFile=open(r'.\datas\반려동물_'+f'{typen}.csv','w+',encoding='utf-8-sig')
+    csvFile=open(r'경로지정'+f'{typen}.csv','w+',encoding='utf-8-sig')
     
     urln = 'https://search.shopping.naver.com/search/all?query=' + typen
     driver.get(urln)
     ###### first crawl the brand name ###############
     
+    #브랜드 영역 조회후 카테고리+ 클릭
     try:
-        toplist = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div/div[2]/div/div[2]')
-        toplist.find_element_by_css_selector('a.filter_btn_extend__31sOH').click()
+        toplist = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div/div[2]/div/div[2]') 
+        toplist.find_element_by_css_selector('a.filter_btn_extend__31sOH').click() 
     except:
         pass
     
+     #카테고리+ 파싱
     beautifulObj=BeautifulSoup(driver.page_source,features="html.parser")
-    
+   
     subobj1 = beautifulObj.select('div.filter_finder_col__3ttPW')
     # print(subobj1)
     # print(len(subobj1))
@@ -59,14 +58,7 @@ for typen in cosmetic:
                         csvFile.write(f'{bb.get_text()},\n')
     except:
         pass
-    # toplist1 = driver.find_element_by_xpath('//*[@id="__next"]/div/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/ul/li[1]/a/span')
-    # print(toplist1.get_text())
-
-    # mm = toplist.find_element_by_css_selector('div.filter_finder_cell__2218d')
-    # name = mm.find_elements_by_css_selector('span.filter_text_over__3zD9c')
-    # print(len(name))
-    # for jj in name:
-    #     print(jj.get_text())
+    
    #########crawl the title of product ######################################################################################
     for pagingindex in range(1,100):
 
@@ -75,16 +67,7 @@ for typen in cosmetic:
             # driver.get(urln)  
             print(pagingindex)
         else:
-            # driver.find_element_by_css_selector('input.searchInput_search_input__3ZswN').send_keys('화장품'+f'{typen}')
-            # print(urln+ f"&pagingIndex={pagingindex}&pagingSize=40&productSet=total&query=화장품{typen}&sort=rel&timestamp=&viewType=list")
-            # urlfollowing = urln+ f"&pagingIndex={pagingindex}&pagingSize=40&productSet=total&query=화장품{typen}&sort=rel&timestamp=&viewType=list"
-            # # &pagingSize=40&productSet=total&query=화장품스킨케어&sort=rel&timestamp=&viewType=list
-            # # &pagingSize=40&productSet=total&query=화장품스킨케어&sort=rel&timestamp=&viewType=list
-            # driver.get(urlfollowing) 
-            # print(2)
-            # if pagingindex == 3:
-            #     time.sleep(1000)
-            pagenumber = driver.find_elements_by_css_selector('a.pagination_btn_page__FuJaU')
+            pagenumber = driver.find_elements_by_css_selector('a.pagination_btn_page__FuJaU') #크롤링이 끝나면 다음페이지로 이동
             bsobj1 = BeautifulSoup(driver.page_source, features= "html.parser")
             pageobj1 = bsobj1.select('a.pagination_btn_page__FuJaU')
             for pagen, button1 in zip(pageobj1,pagenumber):
@@ -104,7 +87,7 @@ for typen in cosmetic:
             
             # html = body.get_attribute('innerHTML')
             bsObj=BeautifulSoup(driver.page_source,features="html.parser")
-            mainobj = bsObj.select('div.basicList_title__3P9Q7 > a') 
+            mainobj = bsObj.select('div.basicList_title__3P9Q7 > a')  #
 
             for mm in mainobj:
                 
